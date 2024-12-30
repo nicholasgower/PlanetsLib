7054d5ecb3e047c5e8158ae1dc9c6e1f5b31ad15
notnotmelon <zacharypicco@gmail.com>
2024-12-30 17:33:13 -0600

    Support for PlanetsLib:is_space_location(planet) and PlanetsLib:steal_music(source_planet, target_planet)

--- lib/planet.lua
+++ lib/planet.lua
@@ -79,3 +79,26 @@ function PlanetsLib.verify_config_fields(config)
             "PlanetsLib:planet_extend() - 'planet_type' field is required. Current allowed values: 'planet', 'moon', 'star'.")
     end
 end
+
+function PlanetsLib:is_space_location(planet)
+    if not planet then return false end
+    return planet.type == "planet" or planet.type == "space-location"
+end
+
+--- Clones music tracks from source_planet to target_planet.
+--- Does not overwrite existing music for target_planet.
+function PlanetsLib:steal_music(source_planet, target_planet)
+    assert(PlanetsLib:is_space_location(source_planet),
+        "PlanetsLib:steal_music() - Invalid parameter 'source_planet'. Field is required to be either `space-location` or `planet` prototype.")
+    assert(PlanetsLib:is_space_location(target_planet),
+        "PlanetsLib:steal_music() - Invalid parameter 'target_planet'. Field is required to be either `space-location` or `planet` prototype.")
+
+    for _, music in pairs(data.raw["ambient-sound"]) do
+        if music.planet == source_planet.name or (music.track_type == "hero-track" and music.name:find(source_planet.name)) then
+            local new_music = table.deepcopy(music)
+            new_music.name = music.name .. "-" .. target_planet.name
+            new_music.planet = target_planet.name
+            data:extend {new_music}
+        end
+    end
+end
