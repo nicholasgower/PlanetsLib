517c6e0988c3b5d2412bf008ace3b9539787d307
danielmartin0 <github@danielgmartin.com>
2024-12-30 23:51:08 +0000

    api.lua

---
+++ api.lua
@@ -0,0 +1,15 @@
+local technology = require("lib.technology")
+local planet = require("lib.planet")
+
+function PlanetsLib:planet_extend(configs)
+    local planets = planet.planet_extend(configs)
+
+    data:extend(planets)
+    return planets
+end
+
+PlanetsLib.technology_icons_planet_cargo_drops = technology.technology_icons_planet_cargo_drops
+PlanetsLib.technology_effect_cargo_drops = technology.technology_effect_cargo_drops
+PlanetsLib.technology_icons_moon = technology.technology_icons_moon
+
+PlanetsLib.borrow_music = planet.borrow_music
--- data.lua
+++ data.lua
@@ -1,20 +1,4 @@
 PlanetsLib = {}
+require("api")
 require("prototypes.surface-property")
 require("prototypes.categories")
-local technology = require("lib.technology")
-local planet = require("lib.planet")
-
---== API ==--
-
-function PlanetsLib:planet_extend(configs)
-	local planets = planet.planet_extend(configs)
-
-	data:extend(planets)
-	return planets
-end
-
-PlanetsLib.technology_icons_planet_cargo_drops = technology.technology_icons_planet_cargo_drops
-PlanetsLib.technology_effect_cargo_drops = technology.technology_effect_cargo_drops
-PlanetsLib.technology_icons_moon = technology.technology_icons_moon
-
-PlanetsLib.borrow_music = planet.borrow_music
