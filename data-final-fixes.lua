fbe041fe5b87912bf08456020f989c2f0503a342
Nicholas Gower <nicholasgower01@gmail.com>
2025-01-01 14:31:58 -0800

    feat: Added optional assertion that planets' gas contents do not exceed 100%. Enabled by default.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -28,3 +28,23 @@ for _, p in pairs(data.raw["space-location"]) do
 		data.raw["space-location"][p.name] = nil
 	end
 end
+
+local gas_list = { "oxygen", "nitrogen", "carbon-dioxide", "argon" }
+local enforce_percentage = settings.startup["PlanetsLib-enforce-gas-percentage"].value --Whether code should assert that combined gas contents add up to less than 100%.
+
+for _, p in pairs(data.raw.planet) do
+	if enforce_percentage then
+		local gas_content = 0
+		for _, gas in pairs(gas_list) do
+			if p.surface_properties[gas] then
+				gas_content = gas_content + p.surface_properties[gas]
+			end
+		end
+		assert(
+			gas_content <= 100,
+			"Combined gas contents of planet "
+				.. p.name
+				.. ' exceed 100%. To override this assertion, add \'data.raw["bool-setting"]["PlanetsLib-enforce-gas-percentage"].forced_value = false\' to data-updates.lua.'
+		)
+	end
+end
--- prototypes/surface-property.lua
+++ prototypes/surface-property.lua
@@ -1,19 +1,2 @@
-data:extend({
-	{
-		type = "surface-property",
-		name = "temperature",
-		default_value = 288,
-	},
-})
-
---== Note: ==--
--- Like with distance measures, these temperatures are milder than astrophysically accurate values. They are midway between astrophysical temperatures and temperatures in which the engineer might plausibly survive.
-
-data.raw.surface["space-platform"].surface_properties.temperature = 268
-data.raw.planet["nauvis"].surface_properties.temperature = 288
-data.raw.planet["vulcanus"].surface_properties.temperature = 332
-data.raw.planet["fulgora"].surface_properties.temperature = 314
-data.raw.planet["gleba"].surface_properties.temperature = 298
-data.raw.planet["aquilo"].surface_properties.temperature = 258
 require("surface-property.temperature")
 require("surface-property.atmosphere")
