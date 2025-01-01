aa5339f55807df224205b4039e31b9a74d461c7a
danielmartin0 <github@danielgmartin.com>
2025-01-01 20:50:29 +0000

    orbits.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,8 +1,8 @@
-local planet = require("lib.planet")
+local orbits = require("lib.orbits")
 
 for _, p in pairs(data.raw.planet) do
     if p.planet_type and p.planet_type == "moon" and p.orbit.parent ~= "star" then
         -- TODO: Should the orientation be rotated if the planet's orbit has rotated?
-        planet.set_position_from_orbit(p, p.orbit)
+        orbits.set_position_from_orbit(p, p.orbit)
     end
 end
--- lib/planet.lua
+++ lib/planet.lua
@@ -1,3 +1,5 @@
+local orbits = require("lib.orbits")
+
 local Public = {}
 
 function Public.planet_extend(configs)
@@ -13,7 +15,7 @@ function Public.planet_extend(configs)
             label_orientation = config.orbit.label_orientation,
         }
 
-        Public.set_position_from_orbit(planet, config.orbit)
+        orbits.set_position_from_orbit(planet, config.orbit)
 
         for k, v in pairs(config) do -- This will not include distance, orientation due to validity checks.
             planet[k] = v
@@ -28,33 +30,6 @@ function Public.planet_extend(configs)
 
     data:extend(planets)
     return planets
-end
-
-function Public.set_position_from_orbit(planet, orbit)
-    if orbit.parent == "star" then
-        planet.distance = orbit.distance
-        planet.orientation = orbit.orientation
-    else
-        local parent = data.raw.planet[orbit.parent]
-        local parent_distance = parent.distance
-        local parent_orientation = parent.orientation
-
-        local parent_angle = parent_orientation * 2 * math.pi
-        local orbit_angle = orbit.orientation * 2 * math.pi
-
-        local px = parent_distance * math.cos(parent_angle)
-        local py = parent_distance * math.sin(parent_angle)
-        local ox = orbit.distance * math.cos(orbit_angle)
-        local oy = orbit.distance * math.sin(orbit_angle)
-
-        local x = px + ox
-        local y = py + oy
-
-        planet.distance = math.sqrt(x * x + y * y)
-        planet.orientation = math.atan2(y, x) / (2 * math.pi)
-        if planet.orientation < 0 then planet.orientation = planet.orientation + 1 end
-        if planet.orientation > 1 then planet.orientation = planet.orientation - 1 end
-    end
 end
 
 function Public.verify_config_fields(config)
