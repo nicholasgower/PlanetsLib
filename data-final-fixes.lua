8c8f5724ce9cf81072ec7bede5f7b7621666fb76
danielmartin0 <github@danielgmartin.com>
2025-01-02 14:27:00 +0000

    see changelog.txt

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -33,18 +33,20 @@ local gas_list = { "oxygen", "nitrogen", "carbon-dioxide", "argon" }
 local enforce_percentage = settings.startup["PlanetsLib-enforce-gas-percentage"].value --Whether code should assert that combined gas contents add up to less than 100%.
 
 for _, p in pairs(data.raw.planet) do
-	if enforce_percentage then
-		local gas_content = 0
-		for _, gas in pairs(gas_list) do
-			if p.surface_properties[gas] then
-				gas_content = gas_content + p.surface_properties[gas]
+	if p.surface_properties then
+		if enforce_percentage then
+			local gas_content = 0
+			for _, gas in pairs(gas_list) do
+				if p.surface_properties[gas] then
+					gas_content = gas_content + p.surface_properties[gas]
+				end
 			end
+			assert(
+				gas_content <= 100,
+				"Combined gas contents of planet "
+					.. p.name
+					.. ' exceed 100%. To override this assertion, add \'data.raw["bool-setting"]["PlanetsLib-enforce-gas-percentage"].forced_value = false\' to data-updates.lua.'
+			)
 		end
-		assert(
-			gas_content <= 100,
-			"Combined gas contents of planet "
-				.. p.name
-				.. ' exceed 100%. To override this assertion, add \'data.raw["bool-setting"]["PlanetsLib-enforce-gas-percentage"].forced_value = false\' to data-updates.lua.'
-		)
 	end
 end
