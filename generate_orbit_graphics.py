74455e3b95d0e1fed2e6dd73934cc963c2d8c19e
danielmartin0 <github@danielgmartin.com>
2025-01-12 19:27:18 +0000

    Update Python script examples

--- generate_orbit_graphics.py
+++ generate_orbit_graphics.py
@@ -1,3 +1,4 @@
 from lib.orbit_graphic_generator import generate_circle
 
 generate_circle(radius=2240, thickness=0.5, width=1, resolution=4480, output_file="orbit-aquilo.png")
+generate_circle(radius=128, thickness=1, width=1, resolution=512, output_file="orbit-muluna.png")
