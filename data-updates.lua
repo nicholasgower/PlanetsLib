116d60132c6c456b5dcea8fbaed1d6a53f2319ce
danielmartin0 <github@danielgmartin.com>
2025-07-20 20:51:47 +0100

    see changelog.txt

--- data-updates.lua
+++ data-updates.lua
@@ -2,5 +2,3 @@ if mods["space-age"] then
 	require("prototypes.override.add-missing-orbit-fields")
 	require("prototypes.override.rocket-silos")
 end
-
-data.raw.technology["logistic-science-pack"].hidden = true
