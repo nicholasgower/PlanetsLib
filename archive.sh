8d8c9e9d5d6a09723b658ff75fd5b3e93cb52768
Nicholas Gower <nicholasgower01@gmail.com>
2025-08-02 14:38:41 -0700

    Raised PlanetsLib version to 1.10.0.

--- archive.sh
+++ archive.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.9.0/ -o PlanetsLib_1.9.0.zip HEAD
+git archive --prefix=PlanetsLib_1.10.0/ -o PlanetsLib_1.10.0.zip HEAD
